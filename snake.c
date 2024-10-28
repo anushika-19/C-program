50fc98f36afd6190acb4be51906b639e8f55da34
Anushika Tyagi <anushikatyagi1@gmail.com>
2024-10-28 14:50:45 +0530

    Snake water gun game

---
+++ snake.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int snakeWaterGun(char you,char comp){
+    if(comp==you){
+        return 0;
+    }
+    if(you=='s' && comp=='g'){
+        return -1;
+    }
+    else if(you=='g' && comp=='s'){
+        return 1;
+    }
+    if(you=='s' && comp=='w'){
+        return -1;
+    }
+    else if(you=='w' && comp=='s'){
+        return 1;
+    }
+    if(you=='g' && comp=='w'){
+        return -1;
+    }
+    else if(you=='w' && comp=='g'){
+        return 1;
+    }
+}
+int main()
+{
+    char you,comp;
+    comp='s';
+    printf("Enter 's' for snake, 'w' for water and 'g' for gun \n");
+    scanf("%c",&you);
+    int result=snakeWaterGun(you,comp);
+    printf("You chose %c and computer chose %c\n",you,comp);
+    if(result==0){
+        printf("Game draw\n");
+    }
+    else if(result==1){
+        printf("You win\n");
+    }
+    else{
+        printf("You lose\n");
+    }
+    return 0;
+}
